26c9d689c1109b9a1a14c0a05ef91af0467227c0
ronritari <ron.ritari@gmail.com>
2021-05-04 14:25:58 +0300

    1.1 version

---
+++ src/Hero.java
@@ -0,0 +1,42 @@
+import java.util.List;
+import java.util.Scanner;
+public class Hero {
+
+    private String name;
+    private double health, Attack1, Attack2, Attack3;
+
+    public Hero(String n, double h, double a1, double a2, double a3){
+        this.name=n;
+        this.health=h;
+        this.Attack1=a1;
+        this.Attack2=a2;
+        this.Attack3=a3;
+
+    }
+    /*public static void pickHero(int x){
+
+        switch(x){
+            case 0:
+                int health = 100;
+                int Attack1 = 10;
+                int Attack2 = 25;
+                int Attack3 = 50;
+                break;
+            case 1:
+                health = 75;
+                Attack1 = 15;
+                Attack2 = 30;
+                Attack3 = 60;
+                break;
+            case 2:
+                health = 150;
+                Attack1 = 5;
+                Attack2 = 20;
+                Attack3 = 40;
+                break;
+        }*/
+        //list.add(type, health, Attack1, Attack2, Attack3); //[type, health, Attack1, Attack2, Attack3]
+        return; //list;
+    }
+
+}
--- src/start.java
+++ src/start.java
@@ -1,13 +1,29 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class start {
-Scanner sc= new Scanner;
+Scanner sc = new Scanner(System.in);
 public void start(){
     boolean gameOver = false;
+    ArrayList<Hero> listOfHeroes = new ArrayList<>();
+
+    Hero knight = new Hero("knight", 100, 10, 25, 50);
+    Hero wizard = new Hero("wizard", 75, 15, 30, 60);
+    Hero bigboi = new Hero("big boi", 150, 5, 20, 40);
+
+    //listOfHeroes.add("knight");
+    //listOfHeroes.add("wizard");
+    //listOfHeroes.add("big boi");
+
+    System.out.println("to choose knight press 0 \nto choose wizard press 1 \nto choose big boi press 2");
+    int x = sc.nextInt();
+    Hero.pickHero(x);
+
+
     while(gameOver = false) {
         System.out.println("Start by choosing your character.");
         System.out.println("Write the number of the hero you would like to choose");
-        hero user = new hero();
+        Hero user = new Hero();
 
         gameOver = true;
         }
